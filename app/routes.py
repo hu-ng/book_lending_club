e5acebaf91c3f1018e4d20658ca89c5474960ea8
Yang Yunfan <yunfan.yang@minerva.kgi.edu>
2019-02-25 23:20:34 +0530

    project structuring

--- app/routes.py
+++ app/routes.py
@@ -6,5 +6,25 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+# Page for all books available 
+@app.route('/book_store')
+def book_store():
+    return render_template('bookstore.html')
+
+# User profile page
+@app.route('/<int:id>/profile')
+def user_profile(id):
+    return render_template('profile.html', id=user_id)
+
+# Book show page
+@app.route('/book/<int:id>')
+def book(id):
+    return render_template('book.html', id=book_id)
+
+# Book lending request page
+@app.route('/transaction/<int:lender_id>/<int:borrower_id>')
+def transaction(lender_id, borrower_id):
+    return render_template('transaction.html', lender=lender_id, borrower_id)
+
 if __name__ == '__main__':
     app.run(debug=True)
