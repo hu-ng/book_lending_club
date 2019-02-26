2a76f22de06bdbbdc4ade8e225c2a99eefc2f54d
Yang Yunfan <yunfan.yang@minerva.kgi.edu>
2019-02-27 00:44:21 +0530

    add database config

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,12 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
+app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://{0}:{1}@{2}/{3}'.format(os.environ['database_username'], os.environ['database_pwd'], os.environ['database_host'], os.environ['database_db'])
+db = SQLAlchemy(app)
+
 
 @app.route('/')
 def index():
