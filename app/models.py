c0cbe51037e966e88a9441392d7c468cebad9354
Yunfan Yang <yunfan.yang@minerva.kgi.edu>
2019-04-03 17:56:25 +0530

    Revert "[Ready to Merge]: Book model and submit forms"

--- app/models.py
+++ app/models.py
@@ -1,54 +1,45 @@
 from app import db, app, login_manager
 from flask_login import UserMixin
 
-
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
 
 
 class User(db.Model, UserMixin):
-    __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key = True)
-    username = db.Column(db.String(40), nullable = False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(60), nullable = False)
-    # Drop down, select field
-    region = db.Column(db.String(60), nullable = False)
+	id = db.Column(db.Integer, primary_key = True)
+	username = db.Column(db.String(40), nullable = False)
+	email = db.Column(db.String(120), unique=True, nullable=False)
+	password = db.Column(db.String(60), nullable = False)
+	
+	books = db.relationship('Book', backref='user', lazy=True)
 
-    books = db.relationship('Book', backref='user', lazy=True)
-
-
-def __repr__(self):
-    return f"User('{self.username}', '{self.email}')"
+	def __repr__(self):
+		return f"User('{self.username}', '{self.email}')"
 
 
 
 class Meta_book(db.Model):
-    __tablename__ = 'metas'
-    id = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String(120), nullable = False)
-    author = db.Column(db.String(40), nullable =False)
-    numpages = db.Column(db.Integer, nullable = False)
+	id = db.Column(db.Integer, primary_key = True)
+	name = db.Column(db.String(), nullable = False)
+	author = db.Column(db.String(), nullable =False)
+	numpages = db.Column(db.Integer, nullable = False)
+	
+	copies = db.relationship('Book', backref='metas', lazy=True)
 
-    copies = db.relationship('Book', backref='metas', lazy=True)
-
-    def __repr__(self):
-        return f"Meta_book('{self.name}', '{self.author}')"
+	def __repr__(self):
+		return f"Meta_book('{self.name}', '{self.author}')"
 
 
-# Region
 class Book(db.Model):
-    __tablename__ = 'book'
-    id = db.Column(db.Integer, primary_key = True)
+	id = db.Column(db.Integer, primary_key = True)
+	
+	metabook_id = db.Column(db.Integer, db.ForeignKey('metas.id'), nullable=False)
+	
+	owner_id =  db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+	
+	availability = db.Column(db.Boolean, nullable = False)
+	condition = db.Column(db.Boolean, nullable = False)
 
-    metabook_id = db.Column(db.Integer, db.ForeignKey('metas.id'), nullable=False)
-
-    owner_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-
-    region = db.Column(db.String(60), db.ForeignKey('users.region'), nullable=False)
-    availability = db.Column(db.Boolean, nullable = False, default=True)
-    condition = db.Column(db.String(60), nullable = False)
-
-    def __repr__(self):
-        return f"Book('{self.name}', '{self.author}')"
+	def __repr__(self):
+		return f"Book('{self.name}', '{self.author}')"
