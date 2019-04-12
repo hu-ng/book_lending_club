2ab7e716f1e3fefb5070730aa4fcd4cb99452e2a
Jason Liang <jjjasonliang@gmail.com>
2019-04-12 17:15:31 +0530

    add book and metabook in utils

--- utils.py
+++ utils.py
@@ -1,5 +1,5 @@
 from app import db, bcrypt, app
-from app.models import User
+from app.models import User, Meta_book, Book
 
 db.drop_all()
 db.create_all()
@@ -9,3 +9,12 @@ a = User(username = "Joe", email = "xd@gmail.com", password=hashed_password, reg
 db.session.add(a)
 
 db.session.commit()
+
+# add book
+meta = Meta_book(name="Book_Meta", author="Mr. XD", numpages="8787")
+db.session.add(meta)
+db.session.commit()
+
+book = Book(metabook_id=Meta_book.query.filter_by(name="Book_Meta", author="Mr. XD").first().id,
+			owner_id=User.query.filter_by(username = "Joe", email = "xd@gmail.com").first().id, 
+            condition="new", region="sf")
