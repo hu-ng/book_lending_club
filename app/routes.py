2392add823ea0a669d32c973fcbf085ce9961780
Jason Liang <jjjasonliang@gmail.com>
2019-04-18 17:28:11 +0530

    remove book and bookstore

--- app/routes.py
+++ app/routes.py
@@ -10,22 +10,10 @@ def index():
     return render_template('index.html')
 
 
-# Page for all books available
-@app.route('/book_store')
-def book_store():
-    return render_template('bookstore.html')
-
-
 # User profile page
 @app.route('/<int:id>/profile')
 def user_profile(id):
     return render_template('profile.html', id=user_id)
-
-
-# Book show page
-@app.route('/book/<int:id>')
-def book(id):
-    return render_template('book.html', id=book_id)
 
 
 # Book lending request page
