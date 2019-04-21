5a2174b227994cf7e9241f189012ce93d45ca780
Jose Flores <flomay@minerva.kgi.edu>
2019-04-21 03:09:01 -0300

    Fixed mistake in send_email

--- app/send_emails.py
+++ app/send_emails.py
@@ -6,18 +6,21 @@ import config
 def send_email(receiver,topic,book_id):
     metabook_id = Book.query.filter_by(id=book_id).first().metabook_id
     name = Meta_book.query.filter_by(id=metabook_id).first().name
-    if topic = "remind"
+    if topic == "remind"
         subject = "Reminder to return Book"
         msg = "This is a reminder that you should return {} in the next 24 hours.".format(name)
     
-    elif topic = "requested"
+    elif topic == "requested"
         subject = "Succesful book request"
         msg = "You have succesfully requested {}.".format(name)
     
-    elif topic = "requesting"
+    elif topic == "requesting"
         subject = "Book requested"
         msg = "Someone has requested {} from you.".format(name)
 
+    else:
+        raise TypeError("The topic wasn't in the options available.")
+        return
     try:
         server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
         server.ehlo()
