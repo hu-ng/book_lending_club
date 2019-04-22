d75e7542e2f0a499bee41a51c4be2bb40a1d5165
Henry Hill <henry.jacob.hill@gmail.com>
2019-04-23 05:06:13 +0530

    Carousel tweaks
    User doesn't see option to borrow their own book
    
    Fixed carousel obscuring the page title - positioning is now consistent with the profile page

--- app/routes.py
+++ app/routes.py
@@ -161,7 +161,7 @@ def book_display():
         availability.append(not Transaction.query.filter_by(status='borrower_confirmed', book_id = book.id).first())
 
     book_items = zip(books, book_names, book_authors, availability)
-    return render_template('display.html', books=list(book_items)) # passing it as a list object allows to iterate over it more than once in Jinja, which may be useful.
+    return render_template('display.html', user = current_user, books=list(book_items)) # passing it as a list object allows to iterate over it more than once in Jinja, which may be useful.
 
 
 @app.route('/borrowing_request/<int:book_id>',methods=["GET", "POST"])
