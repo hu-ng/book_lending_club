cc0c7ba0d2e956fccbc863f467912d9975d80f81
ivannavatamanyuk <46892412+ivannavatamanyuk@users.noreply.github.com>
2019-04-26 05:16:45 +0530

    Update test.py
    Added a test for testing login success

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+import app
 import unittest
 
 class TestStringMethods(unittest.TestCase):
@@ -15,6 +16,12 @@ class TestStringMethods(unittest.TestCase):
         # check that s.split fails when the separator is not a string
         with self.assertRaises(TypeError):
             s.split(2)
+
+class IntegrationTest(unittest.TestCase):
+    def test_loginsuccess(self):
+        r = requests.post('http://localhost:5000/login', data= {'username': "Username", 'password': "12345", 'confirm_password': "12345"})
+        self.assertEqual(r.status_code,200)
+
 
 if __name__ == '__main__':
     unittest.main()
