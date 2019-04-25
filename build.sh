979ed8da1e544556426316b7a4d976063c4513b2
viethoangtranduong <36019296+viethoangtranduong@users.noreply.github.com>
2019-04-26 01:19:29 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,3 +2,5 @@
 # <-t booklending> sets the tag name of the Docker image to booklending
 # <.> where to look for the Dockerfile that is needed for the build at the current directory
 docker build -t booklending .
+
+# Reference: https://medium.freecodecamp.org/docker-easy-as-build-run-done-e174cc452599
