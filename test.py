4006ee443dd63abd88633bf80b4598cda7a32d34
SimonGolombek <35859701+SimonGolombek@users.noreply.github.com>
2019-04-26 19:10:11 +0530

    Adding test for the transaction database

--- test.py
+++ test.py
@@ -131,6 +131,13 @@ class FlaskTestCase(unittest.TestCase):
         self.assertEqual(hydbook.owner_id, 1)
         self.assertEqual(len(numbertorn), 1)
         
+      """
+      The next test checks whether we can add transactions to the database. Unfortunately, it is also possible to add into the database 
+      a transaction where the end date comes before the start date. This is unfortunate and should be fixes, but not a priority as this
+      will likely never happen if new transactions are added programmatically by having the end date say 14 days after the start date.
+      In addition, it is possible to insert a start date that comes before the current date, but this might actually be beneficial as
+      it allows admins to override the database if something unforseen happens.
+      """
     def test_adding_transactions(self):
         t1 = Transaction(book_id=1, borrower_id=1,
                 date_created=datetime.datetime(2019, 5, 26), startdate=datetime.datetime(2019, 4, 16), 
@@ -139,9 +146,10 @@ class FlaskTestCase(unittest.TestCase):
         db.session.commit()
         numtrans = Transaction.query.all()
         self.assertEqual(len(numtrans), 1)
+        #Checking whether the enddate comes before the startdate
+        #self.assertTrue((t1.startdate-t1.enddate).days <= 0)
         #self.assertTrue((now-t1.startdate).days <= 0)
-        self.assertTrue((t1.startdate-t1.enddate).days <= 0)
-          
+     
 if __name__ == '__main__':
     unittest.main()
     
