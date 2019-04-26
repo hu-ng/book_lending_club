6c27cd678479d278930d08265b3a7a2e016ae9cd
SimonGolombek <35859701+SimonGolombek@users.noreply.github.com>
2019-04-26 18:59:07 +0530

    Update test.py

--- test.py
+++ test.py
@@ -5,6 +5,7 @@ import tempfile
 from flask import render_template, request, url_for, flash, redirect
 import unittest
 import requests
+import datetime
 from sqlalchemy import create_engine
 from app import app, bcrypt, db
 from app import routes, forms
@@ -15,6 +16,8 @@ def log_in(self, email, password):
     return requests.post('http://ec2-18-219-248-53.us-east-2.compute.amazonaws.com/login', data=dict(email=email, password=password))
 
 hashed_password = bcrypt.generate_password_hash('password').decode('utf-8')
+
+now = datetime.datetime.now()
 
 
 class FlaskTestCase(unittest.TestCase):
@@ -63,7 +66,7 @@ class FlaskTestCase(unittest.TestCase):
     #Test whether users are able to add books
     def test_user_add_book(self):
         log_in(self, "xd@gmail.com", "111")
-        response = requests.post('http://ec2-18-219-248-53.us-east-2.compute.amazonaws.com/add_books', data=dict(bookname="test book", author="test author", numpages=123, condition="used"))
+        response = requests.post('http://ec2-18-219-248-53.us-east-2.compute.amazonaws.com/add_books', data=dict(bookname="Bible", author="God", numpages=666, condition="torn"))
         self.assertTrue(response.status_code == 200) 
         
     #Setting up a test database to check whether     
@@ -127,6 +130,17 @@ class FlaskTestCase(unittest.TestCase):
         numbertorn = Book.query.filter_by(condition="torn").all()
         self.assertEqual(hydbook.owner_id, 1)
         self.assertEqual(len(numbertorn), 1)
+        
+    def test_adding_transactions(self):
+        t1 = Transaction(book_id=1, borrower_id=1,
+                date_created=datetime.datetime(2019, 5, 26), startdate=datetime.datetime(2019, 4, 16), 
+                         enddate=datetime.datetime(2019, 3, 30), status="open")
+        db.session.add(t1)
+        db.session.commit()
+        numtrans = Transaction.query.all()
+        self.assertEqual(len(numbertrans), 1)
+        self.assertTrue((now-t1.startdate).days <= 0)
+        self.assertTrue((t1.startdate-t1.enddate).days <= 0)
           
 if __name__ == '__main__':
     unittest.main()
